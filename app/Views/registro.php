b6593af5a6af2269119e51d9ce17d16ee4714c6a
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-07-17 16:05:46 -0300

    se agregaron campos al form registro

--- app/Views/registro.php
+++ app/Views/registro.php
@@ -1,21 +1,25 @@
 <section class="pagina-registro min-vh-100 d-flex justify-content-center align-items-center flex-column bg-dark">
     <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
-        <h2 class="text-center mb-4 text-white">Registro</h2>
-        <form class="d-flex flex-column justify-content-center w-50">
+        <h2 class="text-center mb-4 text-white mb-2">Registro</h2>
+        <form class="d-flex flex-column justify-content-center w-25">
             <div class="form-group mb-2">
-                <label for="username" class="text-white">Nombre de Usuario</label>
-                <input type="text" class="form-control" id="username" placeholder="Ingresa tu nombre de usuario">
+                <label for="nombre" class="text-white mb-2">Nombre</label>
+                <input type="text" class="form-control" id="nombre" placeholder="Ingresa tu nombre">
             </div>
             <div class="form-group mb-2">
-                <label for="email" class="text-white ">Correo Electrónico</label>
+                <label for="apellido" class="text-white mb-2">Apellido</label>
+                <input type="text" class="form-control" id="apellido" placeholder="Ingresa tu nombre">
+            </div>
+            <div class="form-group mb-2">
+                <label for="email" class="text-white mb-2 ">Correo Electrónico</label>
                 <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo electrónico">
             </div>
             <div class="form-group mb-2">
-                <label for="password" class="text-white">Contraseña</label>
+                <label for="password" class="text-white mb-2">Contraseña</label>
                 <input type="password" class="form-control" id="password" placeholder="Ingresa tu contraseña">
             </div>
             <div class="form-group mb-2">
-                <label for="confirmPassword" class="text-white ">Confirmar Contraseña</label>
+                <label for="confirmPassword" class="text-white mb-2 ">Confirmar Contraseña</label>
                 <input type="password" class="form-control" id="confirmPassword" placeholder="Confirma tu contraseña">
             </div>
             <button type="submit" class="btn btn-outline-success btn-block mb-2">Registrarse</button>
