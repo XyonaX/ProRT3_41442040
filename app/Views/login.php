033b378eb65f1bbcbf79524a78048e11532a4ffe
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-07-21 20:27:19 -0300

    se agrego el mensaje de error en caso de fallar el inicio de sesion

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,17 +1,22 @@
 <section class="pagina-login min-vh-100 d-flex justify-content-center align-items-center flex-column bg-dark">
     <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
         <h2 class="text-center text-white">Login</h2>
-        <form class="d-flex flex-column justify-content-center w-50">
+        <?php if (session()->getFlashdata('err')) : ?>
+            <div class="alert alert-danger mt-3" role="alert">
+                <?= session()->getFlashdata('err') ?>
+            </div>
+        <?php endif; ?>
+        <?= form_open('login', ['class' => 'd-flex flex-column justify-content-center w-25']) ?>
             <div class="form-group mb-2">
                 <label for="email" class="text-white">Correo Electrónico</label>
-                <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo electrónico">
+                <input type="email" name="email" class="form-control" id="email" placeholder="Ingresa tu correo electrónico">
             </div>
             <div class="form-group mb-2">
                 <label for="password" class="text-white">Contraseña</label>
-                <input type="password" class="form-control" id="password" placeholder="Ingresa tu contraseña">
+                <input type="password" name="password" class="form-control" id="password" placeholder="Ingresa tu contraseña">
             </div>
             <button type="submit" class="btn btn-outline-success btn-block mb-2">Iniciar Sesión</button>
             <span class="text-success">¿No tienes cuenta? <a href="registro" class="text-success link-underline-success link-underline-opacity-50">Registrate</a></span>
-        </form>
+        <?= form_close() ?>
     </div>
 </section>
