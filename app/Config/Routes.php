dcb1d3b78777cdf30cd5c852ab714e19a9aa49a7
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-07-21 20:26:16 -0300

    se agregaron rutas para funciones del controlador

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,5 +11,10 @@ $routes->get('/registro', 'Home::registro_index');
 $routes->get('/login', 'Home::login_index');
 $routes->get('/quienesSomos', 'Home::quienesSomos_index');
 $routes->get('/acercade', 'Home::acercade_index');
-
+$routes->get('/logout', 'UsuariosController::logout_user');
 $routes->post('/registro', 'UsuariosController::registro_usuario');
+$routes->post('/login', 'Login_controller::login_user');
+$routes->post('/usuarios/toggle_estado/(:num)', 'UsuariosController::toggle_estado/$1');
+$routes->get('/ver_usuarios', 'UsuariosController::listar_usuarios');
+$routes->get('/usuarios/editar/(:num)', 'UsuariosController::editar_usuario/$1');
+$routes->post('/usuarios/actualizar/(:num)', 'UsuariosController::actualizar_usuario/$1');
